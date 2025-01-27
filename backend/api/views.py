1baca38a34f2b211e84c7c989f540fd33bb8d417
Muli Job <munyoki912@gmail.com>
2025-01-27 11:53:14 +0300

    echo api endpoint in jsonresponse

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,18 @@
 """API View Functions"""
+import json
 from django.http import JsonResponse
 
 def api_home(request, *args, **kwargs):
     """Home Api"""
-    return JsonResponse({"message": "Mama we made it"})
+    print(request.GET)
+    body = request.body
+    data = {}
+    try:
+        data = json.loads(body)
+    except:
+        pass
+    print(data)
+    data['params'] = dict(request.GET )
+    data['headers'] = dict(request.headers)
+    data['content_type'] = request.content_type
+    return JsonResponse(data)
