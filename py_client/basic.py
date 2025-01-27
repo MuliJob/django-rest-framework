5bfae8e0132cd6148531ac7e1a66a9274681c4f4
Muli Job <munyoki912@gmail.com>
2025-01-27 19:24:03 +0300

    Posting Data with Django Rest Framework Views

--- py_client/basic.py
+++ py_client/basic.py
@@ -5,7 +5,7 @@ import requests
 # endpoint = "https://httpbin.org/anything"
 endpoint = "http://localhost:8000/api/"
 
-get_response = requests.get(endpoint, params={"abc": 123}, json={"query": "Hello World"})
+get_response = requests.post(endpoint, json={"content": "Hello World"})
 # print(get_response.text)
 # print(get_response.status_code)
 
